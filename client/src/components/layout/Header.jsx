f93bcb8f351b3885ca9c604b7e5872e959359110
Manish Singh Bisht <manisbst123@gmail.com>
2025-04-28 23:28:29 +0530

    css

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="logo" onClick={() => navigate("/projects")}>
-        Task Manager
+        Task Tracker
       </div>
       {currentUser && (
         <div className="user-menu">
